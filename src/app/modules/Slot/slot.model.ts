a9fa3d711b6a78ee180c624e55daae232af8f246
NOMAN1802 <mustakimnoman82@gmail.com>
2024-06-14 13:24:29 +0600

    slot interface modelzod validation done

---
+++ src/app/modules/Slot/slot.model.ts
@@ -0,0 +1,44 @@
+import { Schema, model, Document, Types } from 'mongoose';
+import { TSlot } from './slot.interface';
+
+
+
+const slotSchema = new Schema<TSlot>(
+    {
+
+    room:
+    { 
+    type: Schema.Types.ObjectId,
+    ref: 'Room',
+    required: true 
+        },
+    date:
+   { 
+    type: Date, 
+    required: true 
+    },
+    startTime:
+   { 
+    type: String, 
+    required: true 
+    },
+    endTime: 
+    {
+    type: String,
+    required: true
+    },
+    isBooked: { 
+    type: Boolean,
+    default: false
+    },
+
+    },
+    {
+    timestamps: true 
+    }
+    );
+
+
+const Slot = model<TSlot>('Slot', slotSchema);
+
+export default Slot;
