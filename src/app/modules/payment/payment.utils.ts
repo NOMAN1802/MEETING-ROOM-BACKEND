5b9f58d1d6c15701b4d05018c5e256894f5665fe
NOMAN1802 <mustakimnoman82@gmail.com>
2024-09-01 20:13:15 +0600

    final update

--- src/app/modules/payment/payment.utils.ts
+++ src/app/modules/payment/payment.utils.ts
@@ -12,7 +12,7 @@ export const initiatePayment = async (paymentData: any) => {
       tran_id: paymentData.transactionId,
       success_url: `http://localhost:5000/api/payment/confirmation?transactionId=${paymentData.transactionId}&status=success`,
       fail_url: `http://localhost:5000/api/payment/confirmation?status=failed`,
-      cancel_url: 'http://localhost:5173/',
+      cancel_url: 'https://meeting-client.vercel.app/',
       amount: paymentData.totalAmount,
       currency: 'BDT',
       desc: 'Merchant Registration Payment',
